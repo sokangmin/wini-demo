dadb5fc38953efab2a111a935fff93e01bb13ddd
sokangmin <sokangmin@gmail.com>
2022-01-07 09:22:30 +0900

    codecov test

--- src/test/java/com/wini/demo/GreetingControllerTest.java
+++ src/test/java/com/wini/demo/GreetingControllerTest.java
@@ -22,7 +22,7 @@ class GreetingControllerTest {
     @Test
     public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
         this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$.content").value("Hellott, World!"));
+                .andExpect(jsonPath("$.content").value("Hello, World!"));
     }
 
     @Test
